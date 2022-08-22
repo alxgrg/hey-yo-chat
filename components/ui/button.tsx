e71b70e49767e605aafc49736257bc032e6b82c8
alxgrg <alxmgrg@gmail.com>
2022-08-22 15:26:43 -0400

    Add link option to button component and mess wit landing page styles

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from 'next';
 import React from 'react';
 
+import Link from 'next/link';
+
 type Props = {
   children: React.ReactNode;
   color?: string;
+  href?: string;
   onClick?: (
     event: React.MouseEvent<HTMLButtonElement>
   ) => void | Promise<void>;
@@ -12,8 +15,16 @@ type Props = {
 const Button: NextPage<Props> = ({
   children,
   color = 'bg-blue-600',
+  href,
   onClick,
 }) => {
+  if (href) {
+    return (
+      <Link href={href}>
+        <a className={`p-3 text-white rounded ${color}`}>{children}</a>
+      </Link>
+    );
+  }
   return (
     <button onClick={onClick} className={`p-3 text-white rounded ${color}`}>
       {children}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,37 @@ import { useContext } from 'react';
 
 import AuthContext from '../store/auth-context';
 
+import Button from '../components/ui/button';
+import Link from 'next/link';
+
 const Home: NextPage = () => {
   const { currentUser, isLoading } = useContext(AuthContext);
 
   return (
-    <div className='w-1/3'>
-      {!isLoading && (
-        <h1>Welcome {currentUser ? 'back ' + currentUser.email : 'dude!'}!</h1>
-      )}
+    // <div className='w-1/3'>
+    //   {!isLoading && (
+    //     <h1>Welcome {currentUser ? 'back ' + currentUser.email : 'dude!'}!</h1>
+    //   )}
+    // </div>
+    <div className='bg-slate-600 h-[calc(100vh-60px)] flex flex-col items-center'>
+      <div className='text-white mt-12 text-center'>
+        <h1 className='text-3xl text-pink-400'>Hey, Yo!</h1>
+        <p>Create room. Share room. Chat room. </p>
+
+        <div className='flex items-center mt-3'>
+          <div className=''>
+            <Button href='/signin'>Sign in</Button>
+          </div>
+          <div className='p-1' />
+          <p>
+            Or{' '}
+            <Link href='/signup'>
+              <a className='text-pink-400'>create an account</a>
+            </Link>{' '}
+            to get started
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
