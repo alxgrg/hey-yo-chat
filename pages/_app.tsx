4266101d5ad3a7f59bd3320becfe62e4c305d026
alxgrg <alxmgrg@gmail.com>
2022-08-24 17:36:27 -0400

    Create notification context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { AuthContextProvider } from '../store/auth-context';
 import Layout from '../components/layout/layout';
+import { NotificationContextProvider } from '../store/notification-context';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthContextProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <NotificationContextProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </NotificationContextProvider>
     </AuthContextProvider>
   );
 }
---
+++ store/notification-context.tsx
@@ -0,0 +1,60 @@
+import React, { createContext, useState, useEffect, ReactNode } from 'react';
+
+interface Notification {
+  title: string;
+  message: string;
+  status: string;
+}
+
+interface NotificationContextInterface {
+  notification: Notification | null;
+  showNotification: (notificationData: Notification) => void;
+  hideNotification: () => void;
+}
+
+const NotificationContext = createContext({} as NotificationContextInterface);
+
+export const NotificationContextProvider = ({
+  children,
+}: {
+  children: ReactNode;
+}) => {
+  const [activeNotification, setActiveNotification] =
+    useState<Notification | null>(null);
+
+  useEffect(() => {
+    if (
+      (activeNotification && activeNotification.status === 'success') ||
+      activeNotification?.status === 'error'
+    ) {
+      const timer = setTimeout(() => {
+        setActiveNotification(null);
+      }, 2000);
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [activeNotification]);
+
+  const showNotificationHandler = (notificationData: Notification) => {
+    setActiveNotification(notificationData);
+  };
+
+  const hideNotificationHandler = () => {
+    setActiveNotification(null);
+  };
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
+  return (
+    <NotificationContext.Provider value={context}>
+      {children}
+    </NotificationContext.Provider>
+  );
+};
+
+export default NotificationContext;
