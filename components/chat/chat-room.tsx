308e31d14eb18bbc92ecfaae9c0470c61fdb4463
alxgrg <alxmgrg@gmail.com>
2022-08-29 02:19:57 -0400

    Swap chat input for textarea

--- components/chat/chat-room.tsx
+++ components/chat/chat-room.tsx
@@ -65,7 +65,9 @@ const ChatRoom: NextPage = () => {
     });
   }, [currentUser, isLoading, router]);
 
-  const handleMessageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleMessageChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
     setMessage(event.target.value);
   };
 
@@ -138,17 +140,34 @@ const ChatRoom: NextPage = () => {
         )}
         <ScrollToBottom />
       </div>
-      <form onSubmit={handleMessageSubmit} className='flex gap-2 mb-2 w-full'>
-        <Input
-          type='text'
+      <form
+        onSubmit={handleMessageSubmit}
+        className='flex gap-2 mb-2 w-full items-center py-2 px-3 bg-gray-50 rounded-lg dark:bg-gray-700'
+      >
+        <textarea
           name='message'
           required
-          autocomplete='off'
           value={message}
           onChange={handleMessageChange}
-          buttonContent='Send'
-          placeholder='Message...'
-        />
+          rows={1}
+          className='block mx-2 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+          placeholder='Your message...'
+        ></textarea>
+        <button
+          type='submit'
+          className='inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600'
+        >
+          <svg
+            aria-hidden='true'
+            className='w-6 h-6 rotate-90'
+            fill='currentColor'
+            viewBox='0 0 20 20'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path d='M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z'></path>
+          </svg>
+          <span className='sr-only'>Send message</span>
+        </button>
       </form>
     </div>
   );
