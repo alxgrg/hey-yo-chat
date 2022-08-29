f20b7044369c22ce4355f9fc7978ead5ed46f992
alxgrg <alxmgrg@gmail.com>
2022-08-29 14:55:42 -0400

    Fix logo and problematic css

--- components/chat/chat-room.tsx
+++ components/chat/chat-room.tsx
@@ -142,7 +142,7 @@ const ChatRoom: NextPage = () => {
       </div>
       <form
         onSubmit={handleMessageSubmit}
-        className='flex gap-2 mb-2 w-full items-center py-2 px-3 bg-gray-50 rounded-lg dark:bg-gray-700'
+        className='flex gap-2 mb-2 w-full items-center py-2 px-3 rounded-lg bg-gray-700'
       >
         <textarea
           name='message'
@@ -150,12 +150,12 @@ const ChatRoom: NextPage = () => {
           value={message}
           onChange={handleMessageChange}
           rows={1}
-          className='block mx-2 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+          className='block mx-2 p-2.5 w-full text-sm  rounded-lg border bg-gray-800 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500'
           placeholder='Your message...'
         ></textarea>
         <button
           type='submit'
-          className='inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600'
+          className='inline-flex justify-center p-2 rounded-full cursor-pointer  text-blue-500 hover:bg-gray-600'
         >
           <svg
             aria-hidden='true'
